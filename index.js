ee43b310fc93a8855ce76d3a34a337008e9f4fc1
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-15 12:22:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function happyHolidayCountdown(holiday, days) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
